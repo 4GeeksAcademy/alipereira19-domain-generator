1bccc7b03afa7a00b970cafee59d80df7e2970d1
alipereira19 <aliper1908@gmail.com>
2024-11-21 03:55:19 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "his", "her", "their"];
+  let adj = ["great", "big", "amazing", "unstoppable", "red", "blue"];
+  let noun = ["jogger", "racoon", "dog", "merchant", "driver", "player", "god"];
+
+  let allDomains = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        allDomains.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+
+  document.querySelector("#button").addEventListener("click", () => {
+    let position = Math.floor(Math.random() * allDomains.length);
+    document.getElementById("domain").innerHTML = allDomains[position];
+  });
 };
